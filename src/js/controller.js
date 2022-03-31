4fdb8cec3f81d4cc5f639c37c9cd9ba81227d3da
redha-boubakour <redha.boubakour@yahoo.fr>
2022-03-31 19:28:20 +0200

    commit : pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -42,13 +43,23 @@ const controlSearchResults = async function () {
 
     // Render search results
     resultsView.render(model.getSearchResultsPage());
+
+    // Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (error) {
     resultsView.renderError();
   }
 };
 
+const controlPagination = function (goToPage) {
+  // updating the results and the pagination buttons
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
